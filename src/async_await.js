b063e22eda31deb005343002f6ccb7363ab8127e
Tiago Santos <tiago881@gmail.com>
2021-05-03 16:48:34 -0300

    faz uma busca pela API usando function tradicional

--- src/async_await.js
+++ src/async_await.js
@@ -1,8 +1,15 @@
-const getData = async () => {
-  const response = await fetch('https://willianjusten.com.br/search.json');
-  const data = await response.json();
-  // eslint-disable-next-line no-console
-  return data.map((titles) => console.log(titles.title));
-};
+// const getData = async () => {
+//   const response = await fetch('https://willianjusten.com.br/search.json');
+//   const data = await response.json();
+//   // eslint-disable-next-line no-console
+//   return data.map((titles) => console.log(titles.title));
+// };
 
+// getData();
+
+async function getData() {
+  const response = await fetch("https://willianjusten.com.br/search.json");
+  const data = await response.json();
+  return data.map((post) => console.log(post.title));
+}
 getData();
